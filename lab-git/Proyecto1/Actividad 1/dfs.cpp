885e5983716253646b5705b1e04b9e7183a90d46
Erick <ericksilvarodriguez@gmail.com>
2016-10-08 10:46:29 -0400

    Reorganizando cosas un poquito (añadido directorio para actividad 1)

---
+++ lab-git/Proyecto1/Actividad 1/dfs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+long long dfs(int depth, int maxDepth){
+	if (depth == maxDepth) return 0;
+
+	long long count = 0;
+
+	for () //succesores del estado
+
+}
+
+
+
+
+int main(){
+
+
+
+}
