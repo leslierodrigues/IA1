4bc4b33179e08be7010c22a0fb80c8aed12f920b
Erick <ericksilvarodriguez@gmail.com>
2016-10-14 15:27:34 -0400

    Revert "cambiados makefile y completado dfs (make.dfs con el make)"
    This reverts commit c33a50b939d93b35d0b0378a4f87550503b33f52.

--- lab-git/Proyecto1/Actividad 1/dfs.cpp
+++ lab-git/Proyecto1/Actividad 1/dfs.cpp
@@ -1,65 +1,21 @@
 #include <iostream>
-#include <string.h>
-#include <cstdio>
 
 using namespace std;
 
-#define MAX_DEPTH 50
 
+long long dfs(int depth, int maxDepth){
+	if (depth == maxDepth) return 0;
 
-int backwards_dfs(state_t *current, int depth, int max_depth, long long *depth_size,int history){
+	long long count = 0;
 
-	depth_size[depth]++;
+	for () //succesores del estado
 
-	if (depth == max_depth) return 0;
-
-	int ruleID, new_hist;
-	state_t child;
-	ruleid_iterator_t iter;
-
-	init_bwd_iter(&iter,current);
-	while((ruleID = next_ruleid( &iter )) >= 0) {
-		if (!bwd_rule_valid_for_history(history,ruleID)) continue;
-
-		new_hist = next_bwd_history(history,ruleID);
-
-		apply_bwd_rule(ruleID, current, &child);
-
-		backwards_dfs(&child,depth+1,max_depth,depth_size,new_hist);
-	}
-
-	return 0;
 }
 
 
 
 
 int main(){
-	int wanted_depth;
-	long long depth_sizes[MAX_DEPTH];
-	state_t goal;
-	int goalID;
-	int history;
-
-	cout << "Cual es la profundida deseada?" << endl;
-
-	cin >> wanted_depth;
-
-	// We obtain a goal state, shouldnt matter which one we choose.
-	first_goal_state(&goal,&goalID);
-	history = init_history;
-
-	memset(depth_sizes,0,sizeof(depth_sizes));
-
-	backwards_dfs(&goal,0,wanted_depth,depth_sizes,history);
-
-	cout << "Depth" << '\t' << "Moves" << endl;
-	for (int i = 0 ; i <= wanted_depth; i++){
-
-		cout << i << '\t' << depth_sizes[i] << endl;
-
-	}
-
 
 
 
