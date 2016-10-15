91a44948e1d3119faaf9c29424f74f66e881dd2f
Georvic Tur <alexanderstower@gmail.com>
2016-10-15 10:17:57 -0400

    history_len repetido en la tabla del npuzzle.

--- lab-git/Proyecto1/Actividad 1/generar_todas_las_tablas.sh
+++ lab-git/Proyecto1/Actividad 1/generar_todas_las_tablas.sh
@@ -4,7 +4,7 @@
 
 . generar_tablas.sh hanoiPuzzle_Results/hanoiPuzzle_4P_16D_history0_depth12.txt hanoiPuzzle_Results/hanoiPuzzle_4P_16D_history1_depth12.txt | head -n -1 > hanoiPuzzle_Results/hanoiPuzzle_4P_16D_depth12_TABLA.txt
 
-. generar_tablas.sh npuzzle_Results/npuzzle4x4_history0_depth20.txt npuzzle_Results/npuzzle4x4_history1_depth20.txt npuzzle_Results/npuzzle4x4_history0_depth20.txt | head -n -1 > npuzzle_Results/npuzzle4x4_depth20_TABLA.txt
+. generar_tablas.sh npuzzle_Results/npuzzle4x4_history0_depth20.txt npuzzle_Results/npuzzle4x4_history1_depth20.txt npuzzle_Results/npuzzle4x4_history2_depth20.txt | head -n -1 > npuzzle_Results/npuzzle4x4_depth20_TABLA.txt
 
 . generar_tablas.sh topSpinCompactPuzzle_Results/topSpinCompactPuzzle22_4_history0_depth7.txt topSpinCompactPuzzle_Results/topSpinCompactPuzzle22_4_history1_depth7.txt topSpinCompactPuzzle_Results/topSpinCompactPuzzle22_4_history2_depth7.txt | head -n -1 > topSpinCompactPuzzle_Results/topSpinCompactPuzzle22_4_depth7_TABLA.txt
 
