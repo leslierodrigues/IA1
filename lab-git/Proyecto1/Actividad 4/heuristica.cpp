6108719910f5858c5d1dbae80e2537ec63875439
Erick <ericksilvarodriguez@gmail.com>
2016-10-16 16:46:04 -0400

    algunos arreglos a prueba.cpp en la actividad 4

--- lab-git/Proyecto1/Actividad 4/heuristica.cpp
+++ lab-git/Proyecto1/Actividad 4/heuristica.cpp
@@ -3,13 +3,13 @@
 
 using namespace std;
 
-unsigned int heuristic(state_t state){
+unsigned int heuristic(state_t *state){
     
     unsigned int number_not_adjacent = 0;
     int gap;
-    for(unsigned int i=0; i<sizeof(state.vars)/sizeof(state.vars[0]) -1; i++){
+    for(unsigned int i=0; i<sizeof(state->vars)/sizeof(state->vars[0]) -1; i++){
         
-        gap = state.vars[i] - state.vars[i+1]; 
+        gap = state->vars[i] - state->vars[i+1]; 
         if ((gap < -1) || (gap > 1)){
             number_not_adjacent++;
         }
--- lab-git/Proyecto1/Actividad 4/prueba.cpp
+++ lab-git/Proyecto1/Actividad 4/prueba.cpp
@@ -11,35 +11,37 @@ using namespace std;
 
 long long generated_states;
 
-int a_star(state_t*, unsigned int (*heuristica)(state_t));
+int a_star(state_t*);
 
 
 
 // Estructutura que se almacena en la cola de prioridades
 struct node{
-    int priority;
-    state_t s;
+	int priority;
+	state_t s;
+	int cost;
+	int history;
 };
 
 // Se modifica el operador para que la estructura node sea ordenable
 bool operator<(const node &a,const node &b) {
-return a.priority<b.priority;
+return a.priority>b.priority;
 
 }
 
 int main(){
-    
-    state_t start; 
-    string state_string; // Almacena el estado dado por el usuario
-    int result; // Valor retornado por la función
-    int goalID;
-    
-    //map <state_t,int> m; //mapea un estado con su prioridad
-    
-    cout << "Introduzca el estado del problema: " << endl;
-    
-    getline(cin,state_string);
-    if (read_state(state_string.c_str(),&start) == -1){
+	
+	state_t start; 
+	string state_string; // Almacena el estado dado por el usuario
+	int result; // Valor retornado por la función
+	int goalID;
+	
+	//map <state_t,int> m; //mapea un estado con su prioridad
+	
+	cout << "Introduzca el estado del problema: " << endl;
+	
+	getline(cin,state_string);
+	if (read_state(state_string.c_str(),&start) == -1){
 
 		cout << "No pudimos transformar tu input a un estado" << endl;
 		cout << "Puede que este en formato incorrecto." << endl;
@@ -52,12 +54,12 @@ int main(){
 		cout << endl;
 		return 0;
 	}
-    
-    generated_states = 0;
-    
+	
+	generated_states = 0;
+	
 	clock_t begin = clock();
 	
-	result = a_star(&start, &heuristic);
+	result = a_star(&start);
 
 	clock_t end = clock();
 
@@ -66,78 +68,78 @@ int main(){
 
 	cout << "algorithm, heuristic, domain, instance, cost, h0, generated, time, gen_per_sec " << endl;
 
-	cout << " A*, gap, pancake28,\"" << state_string << "\", " << result << ", " << heuristic(start);
+	cout << " A*, gap, pancake28,\"" << state_string << "\", " << result << ", " << heuristic(&start);
 	cout << ", " << generated_states << ", "  << elapsed_secs << ", ";
 	cout << gen_per_sec << endl;
-    
-    return 0;
+	
+	return 0;
 }
 
-int a_star(state_t *start, unsigned int (*heuristica)(state_t)){
-    
+int a_star(state_t *start){
+
+	state_t child,current_state;
+	unsigned int h;
+	int cost, history;
+	int ruleID, new_hist;
+	ruleid_iterator_t iter;
+
 	if (is_goal(start)) return 0;
 	
 	state_map_t *stateMap = new_state_map(); // Map con clave un estado y valor un entero 
 
-    priority_queue<node> q; // Cola de prioridad
-    
-    struct node n;
-    n.priority = 0; // Se calula la prioridad 
-    n.s = *start;   // Se almacena el estado 
-    
-    state_map_add(stateMap,start,0); // Se mapea el nodo con la prioridad
-    q.push(n); // Se inserta en la cola
-    
-    while (!q.empty()){
-        n = q.top(); //Se guarda el primer elemento de la cola
-        q.pop(); // Se desencola 
-        state_t state = n.s; // Se almacena el estado desencolado
-       
-        // Eliminación de duplicados retardado
-    if (n.priority < *state_map_get(stateMap,&state)){
-            state_map_add(stateMap,&state,n.priority); //Se modifica la prioridad del estado en el Map
-            if (is_goal(&state)) return n.priority; 
-            
-            int aux;
-	        int ruleID, new_hist;
-	        state_t child;
-	        ruleid_iterator_t iter;
+	priority_queue<node> q; // Cola de prioridad
+	
+	struct node n;
+	n.priority = heuristic(start); // Se calula la prioridad
+	n.cost = 0;
+	n.history = init_history;
+	n.s = *start;   // Se almacena el estado 
+	
+	state_map_add(stateMap,start,n.cost); // Se mapea el nodo con la prioridad
+	q.push(n); // Se inserta en la cola
+	
+	while (!q.empty()){
+		n = q.top(); //Se guarda el primer elemento de la cola
+		q.pop(); // Se desencola 
+		current_state = n.s; // Se almacena el estado desencolado
 
-	        init_fwd_iter(&iter,&state);
-	        //Expansión de nodos
-	        while((ruleID = next_ruleid( &iter )) >= 0) {
 
-		        if (!fwd_rule_valid_for_history(history,ruleID)) continue;
+		// Eliminación de duplicados retardado
+		if (n.cost <= *state_map_get(stateMap,&current_state)){
+			state_map_add(stateMap,&current_state,n.cost); //Se modifica la prioridad del estado en el Map
+			if (is_goal(&current_state)) return n.cost; 
+			
 
-		        //if (cost + 1 >= max_cost) return -1;
-		        
-		        new_hist = next_fwd_history(history,ruleID);
+			init_fwd_iter(&iter,&current_state);
+			//Expansión de nodos
+			while((ruleID = next_ruleid( &iter )) >= 0) {
 
-		        apply_fwd_rule(ruleID, &state, &child);
-		        generated_states++;
+				if (!fwd_rule_valid_for_history(n.history,ruleID)) continue;
+				
+				new_hist = next_fwd_history(n.history,ruleID);
 
-		        // Chequeamos si el nuevo hijo es un goal.
-		        if (is_goal(&child)) return n.priority + 1;
-                
-                unsigned int h = heuristica(child);
-                
-                if (h < UINT_MAX-1) {
-                    struct node nodoHijo;
-                    nodoHijo.priority = *state_map_get(stateMap,&state) + get_fwd_rule_cost( ruleID ) + h;
-                    nodoHijo.s = child;
-                    state_map_add(stateMap,&child,nodoHijo.priority);
-                    q.push(nodoHijo);
-                }
-                
-		        //aux = dfs(&child,cost+1,max_cost,new_hist);
+				apply_fwd_rule(ruleID, &current_state, &child);
+				generated_states++;
 
-		        //if (aux != -1) return aux;
-	        }
-        }
-       
-    }
-    
-    return -1;
+				// Chequeamos si el nuevo hijo es un goal.
+				if (is_goal(&child)) return n.cost + 1;
+				
+				h = heuristic(&child);
+				
+				if (h < UINT_MAX-1) {
+					struct node nodoHijo;
+					nodoHijo.priority = n.cost + get_fwd_rule_cost( ruleID ) + h;
+					nodoHijo.s = child;
+					nodoHijo.cost = n.cost + get_fwd_rule_cost( ruleID );
+					nodoHijo.history = new_hist;
+					state_map_add(stateMap,&child,nodoHijo.cost);
+					q.push(nodoHijo);
+				}
+			}
+		}
+	}
+	
+	return -1;
 }
 
-//1 2 3 4 6 16 11 10 5 8 7 9 14 12 15 13
+//5 6 10 11 14 12 13 18 17 16 15 3 0 1 2 26 25 24 23 4 19 9 8 7 20 21 22 27
