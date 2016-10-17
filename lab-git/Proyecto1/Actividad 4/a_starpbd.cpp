bb9093f09cf398e9bb9aa8793c640421d8c8a191
Erick <ericksilvarodriguez@gmail.com>
2016-10-16 23:15:25 -0400

    pequenos cambios en a_starpbd para que corra con pdb 5+5+5, falta generalizar

--- lab-git/Proyecto1/Actividad 4/a_starpbd.cpp
+++ lab-git/Proyecto1/Actividad 4/a_starpbd.cpp
@@ -4,13 +4,13 @@
 #include <queue>
 #include <map>
 #include <climits>
-#include "heuristica.cpp"
 
 using namespace std;
 
 long long generated_states;
 
 int a_star(state_t*);
+int heuristic(state_t*);
 
 // Estructutura que se almacena en la cola de prioridades
 struct node{
@@ -25,6 +25,9 @@ bool operator<(const node &a,const node &b) {
 return a.priority>b.priority;
 
 }
+
+state_map_t * grupo1, *grupo2, *grupo3;
+abstraction_t *abs1, *abs2, *abs3;
 
 int main(){
 	
@@ -54,6 +57,23 @@ int main(){
 	
 	generated_states = 0;
 	
+	abs1 = read_abstraction_from_file("Abstracciones/PDB1/grupo1.abst");
+	abs2 = read_abstraction_from_file("Abstracciones/PDB1/grupo2.abst");
+	abs3 = read_abstraction_from_file("Abstracciones/PDB1/grupo3.abst");
+
+
+	FILE* faux = fopen("Abstracciones/PDB1/grupo1.pdb", "r");
+	grupo1 = read_state_map(faux);
+	fclose(faux);
+
+	faux = fopen("Abstracciones/PDB1/grupo2.pdb", "r");
+	grupo2 = read_state_map(faux);
+	fclose(faux);
+
+	faux = fopen("Abstracciones/PDB1/grupo3.pdb", "r");
+	grupo3 = read_state_map(faux);
+	fclose(faux);
+
 	clock_t begin = clock();
 	
 	result = a_star(&start);
@@ -136,3 +156,30 @@ int a_star(state_t *start){
 }
 
 //5 6 10 11 14 12 13 18 17 16 15 3 0 1 2 26 25 24 23 4 19 9 8 7 20 21 22 27
+
+// 8 16 9 3 14 7 4 13 12 1 5 11 10 6 2 15
+int heuristic(state_t *state){
+
+	int heuristicValue = 0;
+	int *aux;
+	state_t abstract_transform;
+
+
+	abstract_state(abs1, state, &abstract_transform);
+	aux = state_map_get(grupo1, &abstract_transform);
+	heuristicValue += *aux;
+
+	assert(aux != NULL);
+	abstract_state(abs2, state, &abstract_transform);
+	aux = state_map_get(grupo2, &abstract_transform);
+
+	heuristicValue += *aux;
+
+	abstract_state(abs3, state, &abstract_transform);
+	aux = state_map_get(grupo3, &abstract_transform);
+
+	heuristicValue += *aux;
+
+	return heuristicValue;
+
+}
