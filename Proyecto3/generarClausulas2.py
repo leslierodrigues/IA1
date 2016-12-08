3dff7a7ab636eb20861bdffeb9e7356f32557b59
Leslie <leslierodrigs@gmail.com>
2016-12-08 03:19:34 +0000

    Se generan las clausulas tipo1

--- Proyecto3/generarClausulas2.py
+++ Proyecto3/generarClausulas2.py
@@ -1,10 +1,22 @@
+
+
+#  bordesDeCeldas,es un arreglo de 3 dimensiones 
+#  1era dimension: fila en la que se encuentra la celda 
+#  2da dimension: columna en la que se encuentra la celda 
+#  3era dimension: borde de la celda
+#
+#  bordesDeCeldas[i][j] es una arreglo de tamano 4 que corresponden a cada una
+#                       de las lineas que bordean la celda, en el siguiente orden:
+#                       norte, sur, este, oeste
+bordesDeCeldas = []
+
 N=5 #Filas
 numeroColumnas = 5
 M=5 #Columnas
 numeroFilas = 5
 
 tablero = [] #Definido para que sea una global
-
+clausulas = [] # Variable global que almacena las clausulas
 
 # Lee una linea.
 def leerLinea():
@@ -32,12 +44,20 @@ mapaCeldaRestriccionNumeroBordes = {(0,1) : 2, (0,2) : 2, (0,4) : 3
                                    ,(3,0) : 1, (3,3) : 0, (3,4) : 3
                                    ,(4,1) : 2, (4,2) : 2, (4,3) : 3}
 
-
+# Niega una variable
 def negar(nombreVariable) :
     if nombreVariable == "" :
         return ""
-    return "-"+nombreVariable 
+    return "-" + nombreVariable 
+
+# equivalente: Dadas dos variables var1 y var2, que representan booleanos,
+# detemina si son equivalentes.
+# 
+# Si ambas comienzan con '-' o ninguna lo hace, entonces son equivalentes. 
  
+def equivalente(var1,var2):
+    return (var1[0]== '-' and var1[0] == var2[0]) or (var1[0] != '-'and var2[0] != var1[0])
+        
 
 '''
 Cláusulas tipo 0
@@ -64,23 +84,71 @@ q(i,j,n) <=> q(i,j+1,s)
 # por cada borde una celda. 
 # Siendo generadas 4 x numero de celdas
 #
-#  retorna: bordesDeCeldas, un arreglo de 3 dimensiones 
-#  1era dimension: fila en la que se encuentra la celda 
-#  2da dimension: columna en la que se encuentra la celda 
-#  3era dimension: borde de la celda
-#
-#  bordesDeCeldas[i][j] es una arreglo de tamano 4 que corresponden a cada una
-#                       de las lineas que bordean la celda, en el siguiente orden:
-#                       norte, sur, este, oeste
 
 def generarVariablesBordesDeCeldas():
-                
-    bordesDeCeldas = [[[]]]
-    variable = 0;
+    variable = 1
+    # -1 indica que no hay nada
+    
+    bordesDeCeldas = [[[0 for k in range(4)] for j in range(M)] for i in range(N)]              
     for i in range(N):
         for j in range(M):
             for k in range(4):
-                bordesDeCeldas = variable
-                variable++ 
-                
+                bordesDeCeldas[i][j][k] = str(variable)
+                variable+=1
+                   
     return bordesDeCeldas
+
+# q, funcion que retorna una variable correspondiente a la unformacion dada
+#
+# i : numero de fila en la que esta la celda
+# j : numero de columna en la que esta la celda
+# k : carater que representa uno de los bordes de la celda
+#      'n': norte
+#      's': sur
+#      'e': este
+#      'w': oeste
+
+def q(i,j,k):
+    if k == 'n':
+        k = 0
+    elif k == 's':
+        k = 1
+    elif k == 'e':
+        k = 2
+    elif k == 'w':
+        k = 3
+        
+    return bordesDeCeldas[i][j][k]
+
+
+# clausulasTipo1, genera las clausulas de tipo 0
+#
+# ejemplo de traduccion de una restriccion:
+#       q(i,j+1,e) <=> q(i,j,w)
+#       q(i,j+1,e) => q(i,j,w) /\ q(i,j+1,e) <= q(i,j,w)
+#       (no q(i,j+1,e) \/ q(i,j,w)) /\ (q(i,j+1,e) \/ q(i,j,w))
+#
+
+def clausulasTipo0():
+      
+    for i in range(N):
+        for j in range(M):
+            # Si se tienen celdas a la izquierda
+            if j < N - 1:
+                clausulas.append(" ".join([negar(q(i,j+1,'e')),
+                                           (q(i,j,'w'))]))
+            # Si se tienen celdas a la derecha
+            if i < M -1:
+                clausulas.append(" ".join([negar(q(i+1,j,'n')),
+                                       (q(i,j,'s'))]))                           
+
+    return clausulas
+
+################################################################################
+# Para la ejecucion  ----------------------------------------------------------#
+
+bordesDeCeldas = generarVariablesBordesDeCeldas() 
+clausulas = clausulasTipo0()
+
+
+
